cf03e8098a9ea3ae0a9115c2de7c8748976224a9
laurel Bonal <laurelbonal@gmail.com>
2024-02-07 17:09:57 -0700

    Add functions for hobbit.js

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,8 +1,33 @@
+function createHobbit(hobbitName, hobbitAge){
+  hobbit = {
+    name: hobbitName,
+    age: hobbitAge,
+    isAdult: false,
+    isOld: false
+  }
+  if (hobbitAge === undefined){
+    hobbit.age = 0
+  }
+  if (hobbitName === undefined){
+    hobbit.name = "unknown"
+  }
+  return hobbit
+}
 
+function celebrateBirthday(hobbit) {
+  hobbit.age += 1;
+  if (hobbit.age > 32) {
+    hobbit.isAdult = true;
+  }
+  else if (hobbit.age > 100){
+    hobbit.isOld = true
+  }
+  return hobbit;
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
+  createHobbit, 
+  celebrateBirthday, 
   // getRing, 
   // meetPeople, 
   // findFriends
